d59400ff9008523bc087f44e1adcb2886fd518ab
blu133721 <73272097+blu133721@users.noreply.github.com>
2020-10-29 19:22:04 +0200

    .

---
+++ index.js
@@ -0,0 +1,10 @@
+const Discord = requrie('discord.js')
+const client = new Discord.Client()
+const prefix = "c/"
+client.once(ready, () => {
+    console.log("Activ")
+})
+
+client.login("")
+/*Acum o sa ii dau upload la github
+*///o sa vezi de ce
