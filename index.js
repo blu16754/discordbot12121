6c65ee9fa16e891d57aeb4f3bf00ad041f8cbab6
blu133721 <73272097+blu133721@users.noreply.github.com>
2020-10-30 19:31:24 +0200

    lol

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 const Discord = requrie('discord.js')
 const client = new Discord.Client()
+const http = require('http')
+const express = require('express')
+const app = express()
 const prefix = "c/"
+// k
+var server = http.createServer(app)
+app.get("/", (request, respone) => {
+    console.log('Test test test => Ping')
+    respone.sendStatus(200);
+
+})
+app.listen(process.env.PORT)
+setInterval(() => {
+    http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`)
+})
+
 client.once(ready, () => {
     console.log("Activ")
 })
